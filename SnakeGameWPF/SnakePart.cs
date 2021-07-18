08920512f67ed5271eb66c418a3dc1370a20d5ef
lamnguyenduy <nguyenduylam29@gmail.com>
2021-07-19 00:17:56 +0700

    Upload full Snake game WPF

---
+++ SnakeGameWPF/SnakePart.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace SnakeGameWPF
+{
+    class SnakePart 
+    { 
+        public UIElement UiElement { get; set; }
+
+        public Point Position { get; set; }
+
+        public bool IsHead { get; set; }
+    }
+}
